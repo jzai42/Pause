0d8a99127dfc765e6e061e9a15362a37f4f9a197
Yifan Zhou <jeremy@MacBook-ProYF-2.local>
2026-01-16 14:21:08 +0700

    chore: track capacitor config, iOS project, and lockfile

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jzai42.pause',
+  appName: 'Pause',
+  webDir: 'dist'
+};
+
+export default config;
